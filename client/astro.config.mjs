adf2d9a5f3d96ad21bf6a9a3099f3e37442752e7
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-11-05 16:01:41 -0500

    added astro libsql db | removed database shell script

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -3,15 +3,17 @@ import preact from '@astrojs/preact';
 
 import tailwind from '@astrojs/tailwind'
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact({ compat: true }), tailwind()],
-	vite: {
-		resolve: {
-			alias: {
-				react: 'preact/compat',
-				'react-dom': 'preact/compat',
-			},
-		},
-	},
+    integrations: [preact({ compat: true }), tailwind(), db()],
+    vite: {
+        resolve: {
+            alias: {
+                react: 'preact/compat',
+                'react-dom': 'preact/compat',
+            },
+        },
+    },
 });
