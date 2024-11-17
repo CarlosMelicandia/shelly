466f16e03b706c7c4bd6b2afb3ddbcd017383f6a
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-11-16 19:04:30 -0500

    added functionality when using db to be able to retrieve data back

--- server/internal/tools/db.go
+++ server/internal/tools/db.go
@@ -9,28 +9,34 @@ import (
 	"github.com/weareinit/Opal/internal/config"
 )
 
-type LoadDBFunc func(db *sql.DB) error
+type LoadDBFunc[T any] func(db *sql.DB) (T, error)
 
-func LoadDB(operation LoadDBFunc) error {
-  envConfig := config.LoadEnv()
+func LoadDB[T any](operation LoadDBFunc[T]) (T, error) {
+    envConfig := config.LoadEnv()
 
-  url := fmt.Sprintf("libsql://%s?authToken=%s", envConfig.TursoDatabaseName, envConfig.TursoAuthToken)
+    url := fmt.Sprintf("libsql://%s?authToken=%s", envConfig.TursoDatabaseName, envConfig.TursoAuthToken)
 
-  db, err := sql.Open("libsql", url)
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
-    os.Exit(1)
-  }
+    db, err := sql.Open("libsql", url)
+    if err != nil {
+        var empty T
+        fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+        os.Exit(1)
+        return empty, err
+    }
 
-  if err := db.Ping(); err != nil {
-      return fmt.Errorf("failed to connect to the database: %v", err)
-  }
+    if err := db.Ping(); err != nil {
+        var empty T
+        return empty, fmt.Errorf("failed to connect to the database: %v", err)
+    }
 
-  defer db.Close()
+    defer db.Close()
 
-  if err := operation(db); err != nil {
-      fmt.Fprintf(os.Stderr, "operation failed: %s\n", err)
-  }
+    result, err := operation(db)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "operation failed: %s\n", err)
+        return result, err
+    }
 
-  return nil
+    return result, nil
 }
+
