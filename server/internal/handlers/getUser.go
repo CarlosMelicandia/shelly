729cbac2ce64e355381e5ae92d853adc0b60b8d6
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-11-22 16:24:06 -0500

    renamed file for better understandability

--- server/internal/handlers/getUser.go
+++ server/internal/handlers/getUser.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/weareinit/Opal/internal/helpers/users"
+	"github.com/weareinit/Opal/internal/helpers/user"
 )
 
-func UserHandler(w http.ResponseWriter, r *http.Request) {
-  user, err := users.GetUser(w, r)
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
+  user, err := user.GetUser(w, r)
 
   if err != nil {
     http.Error(w, "Could not fetch the users information", http.StatusNotFound)
