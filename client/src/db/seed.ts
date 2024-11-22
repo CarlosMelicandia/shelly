4f940503f8666d6cdd02a771db5fc6163634daae
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2024-11-22 16:21:23 -0500

    updated seed. now can control how many people are updated

--- client/src/db/seed.ts
+++ client/src/db/seed.ts
@@ -2,6 +2,8 @@ import { hackerApplications, eventsTracker, user, applicationStatusEnums } from 
 import { db } from './index';
 
 async function seedDatabase() {
+  const firstNames = ["Gabriel", "John", "Jack", "Kyle", "Bruna", "jacob", "david", "Jose", "osmany", "Morgan", "Allen", "Nathan", "chris", "Christian", "Dennis"]
+  const lastNames = ["Pedroza", "Smith", "Saint", "Jones", "gomez", "Doe", "yi", "Yang", "Dennis", "Alexander", "Alexandra", "Gentil"]
 
   // reseed db
   await Promise.all([
@@ -10,27 +12,34 @@ async function seedDatabase() {
     db.delete(eventsTracker)
   ]);
 
+  // keep in mind that this affects storage quota for the database. keep the total iterations at a moderate number
+for (let i = 1; i <= 2; i++) {
+  const firstName = firstNames[i % firstNames.length];
+  const lastName = lastNames[i % lastNames.length];
+  const date = new Date();
+
   try {
     await db.insert(user).values({
-      userId: '123',
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      discordUsername: 'johnnyD',
+      userId: i,
+      firstName,
+      lastName,
+      email: `${firstName}${lastName}${i}@gmail.com`,
+      discordId: `${firstName}${lastName}${i}`,
     });
 
     await db.insert(hackerApplications).values({
-      userId: 'user-123',
-      firstName: 'John',
-      lastName: 'Doe',
-      age: 22,
+      userId: i,
+      firstName,
+      lastName,
+      age: ~~(Math.random() * 50) + 1,
       school: 'Sample University',
       major: 'Computer Science',
       gradYear: 2024,
       levelOfStudy: 'Undergraduate',
       country: 'USA',
-      email: 'johndoe@example.com',
-      phoneNumber: '+1234567890',
-      resumePath: '/resumes/johndoe.pdf',
+      email: `${firstName}${lastName}${i}@gmail.com`,
+      phoneNumber: '1234567890',
+      resumePath: 'some_s3_bucket_path',
       github: 'https://github.com/johndoe',
       linkedin: 'https://linkedin.com/in/johndoe',
       isInternational: false,
@@ -39,20 +48,22 @@ async function seedDatabase() {
       ethnicity: 'Non-Hispanic White',
       avatar: 1,
       agreedMLHNews: true,
-      applicationStatus: applicationStatusEnums[0], // 'registered'
-      createdAt: new Date()
+      applicationStatus: applicationStatusEnums[~~(Math.random() * 6)],
+      createdAt: date,
+      updatedAt: date,
     });
 
     await db.insert(eventsTracker).values({
-      eventId: 'event-001',
+      eventId: i,
       userId: 'user-123',
-      timestamp: new Date()
+      timestamp: date,
     });
 
-    console.log('Database seeded successfully.');
   } catch (error) {
     console.error('Error seeding the database:', error);
   }
+}
+    console.log("Successfully seeded database! 🌱")
 }
 
 seedDatabase();
